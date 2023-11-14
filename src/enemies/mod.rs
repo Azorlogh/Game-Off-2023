38a690e79a339a1bd43c40532914d8ba6884fd87
Azorlogh <bott.alix@gmail.com>
2023-11-14 21:27:48 +0100

    enemy chase behavior & animation

--- src/enemies/mod.rs
+++ src/enemies/mod.rs
@@ -1,10 +1,16 @@
 mod model;
 
-use bevy::prelude::*;
-use bevy_rapier3d::prelude::{Collider, RigidBody};
+use bevy::{
+	math::{Vec3Swizzles, Vec4Swizzles},
+	prelude::*,
+};
+use bevy_rapier3d::{
+	dynamics::{LockedAxes, Velocity},
+	prelude::{Collider, RigidBody},
+};
 
 use self::model::EnemyModelPlugin;
-use crate::player::Player;
+use crate::{health::Health, player::Player};
 
 pub struct EnemyPlugin;
 impl Plugin for EnemyPlugin {
@@ -13,7 +19,7 @@ impl Plugin for EnemyPlugin {
 			.register_type::<EnemyState>()
 			.add_event::<SpawnEnemy>()
 			.add_systems(Startup, setup)
-			.add_systems(Update, (enemy_spawn, enemy_start_chase));
+			.add_systems(Update, (enemy_spawn, enemy_start_chase, enemy_chase));
 	}
 }
 
@@ -31,6 +37,15 @@ pub enum EnemyState {
 	Attack(Entity),
 }
 
+impl EnemyState {
+	pub fn chasing_target(&self) -> Option<Entity> {
+		match self {
+			EnemyState::Idle => None,
+			EnemyState::Attack(target) => Some(*target),
+		}
+	}
+}
+
 fn setup(mut ev_spawn_enemy: EventWriter<SpawnEnemy>) {
 	ev_spawn_enemy.send(SpawnEnemy {
 		pos: Vec3::new(0.0, 0.0, -5.0),
@@ -42,9 +57,17 @@ fn enemy_spawn(mut cmds: Commands, mut ev_spawn_enemy: EventReader<SpawnEnemy>) 
 		cmds.spawn((
 			Name::new("Enemy"),
 			Enemy,
-			SpatialBundle::from_transform(Transform::from_translation(ev.pos)),
+			SpatialBundle::from_transform(
+				Transform::from_translation(ev.pos).with_scale(Vec3::splat(0.5)),
+			),
 			RigidBody::Dynamic,
 			EnemyState::Idle,
+			Velocity::default(),
+			LockedAxes::ROTATION_LOCKED,
+			Health {
+				current: 5.0,
+				max: 10.0,
+			},
 		))
 		.with_children(|cmds| {
 			cmds.spawn((
@@ -73,9 +96,30 @@ fn enemy_start_chase(
 	}
 }
 
-// fn enemy_chase(
-// 	q_global_transform: Query<&GlobalTransform>,
-// 	q_enemies: Query<&EnemyState>,
-// ) {
-// 	for enemy_state in q_enemies.iter().map(||)
-// }
+const ENEMY_SPEED: f32 = 7.0;
+
+fn enemy_chase(
+	time: Res<Time>,
+	q_global_transform: Query<&GlobalTransform>,
+	mut q_enemies: Query<(&EnemyState, (Entity, &mut Transform, &mut Velocity))>,
+) {
+	for (target, (enemy_entity, mut enemy_tr, mut vel)) in q_enemies
+		.iter_mut()
+		.filter_map(|(state, comp)| state.chasing_target().map(|t| (t, comp)))
+	{
+		let enemy_gtr = q_global_transform.get(enemy_entity).unwrap();
+		let target_gtr = q_global_transform.get(target).unwrap();
+		let to_target_dir = (target_gtr.translation() - enemy_gtr.translation()).normalize()
+			* Vec3::new(1.0, 0.0, 1.0);
+
+		let axis = enemy_gtr.forward().cross(to_target_dir);
+		enemy_tr.rotate(Quat::from_scaled_axis(axis * 0.1));
+
+		enemy_tr.translation += to_target_dir * ENEMY_SPEED * time.delta_seconds();
+
+		let linvel = vel.linvel;
+		vel.linvel += (to_target_dir * -(to_target_dir.xz().dot(linvel.xz()).max(0.0)))
+			.extend(0.0)
+			.xzy();
+	}
+}
--- src/enemies/model.rs
+++ src/enemies/model.rs
@@ -1,15 +1,23 @@
+use std::{f32::consts::TAU, time::Duration};
+
 use bevy::{prelude::*, utils::HashMap};
 
-use super::Enemy;
+use super::{Enemy, EnemyState};
 
 pub struct EnemyModelPlugin;
 impl Plugin for EnemyModelPlugin {
 	fn build(&self, app: &mut App) {
-		app.add_systems(Update, (add_enemy_models, enemy_animate));
+		app.add_systems(
+			Update,
+			(add_enemy_models, enemy_animate, enemy_update_animation),
+		);
 	}
 }
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(Component)]
+pub struct AnimationPlayerLink(Entity);
+
+#[derive(Debug, PartialEq, Eq, Hash, Component)]
 pub enum AnimationState {
 	Idle,
 	Run,
@@ -28,6 +36,7 @@ fn add_enemy_models(
 			cmds.spawn((
 				SceneBundle {
 					scene: asset_server.load("models/characters/rat.gltf#Scene0"),
+					transform: Transform::from_rotation(Quat::from_rotation_y(TAU / 2.0)),
 					..default()
 				},
 				ModelAnimations(
@@ -44,24 +53,61 @@ fn add_enemy_models(
 					.into_iter()
 					.collect(),
 				),
+				AnimationState::Idle,
 			));
 		});
 	}
 }
 
 fn enemy_animate(
-	q_model_animations: Query<&ModelAnimations>,
+	mut cmds: Commands,
+	q_model_animations: Query<(Entity, &ModelAnimations)>,
 	q_parent: Query<&Parent>,
 	mut q_added_animation_players: Query<(Entity, &mut AnimationPlayer), Added<AnimationPlayer>>,
 ) {
 	for (anim_player_entity, mut anim_player) in &mut q_added_animation_players {
-		if let Ok(animations) = q_parent
+		if let Ok((model_entity, animations)) = q_parent
 			.get(anim_player_entity)
 			.and_then(|p| q_parent.get(p.get()))
 			.and_then(|p| q_model_animations.get(p.get()))
 		{
+			cmds.entity(model_entity)
+				.insert(AnimationPlayerLink(anim_player_entity));
 			anim_player
-				.play(animations.0[&AnimationState::Run].clone_weak())
+				.play(animations.0[&AnimationState::Idle].clone_weak())
+				.repeat();
+		}
+	}
+}
+
+fn enemy_update_animation(
+	q_enemies: Query<&EnemyState>,
+	mut q_models: Query<(
+		&Parent,
+		&AnimationPlayerLink,
+		&mut AnimationState,
+		&ModelAnimations,
+	)>,
+	mut q_anim_player: Query<&mut AnimationPlayer>,
+) {
+	for (parent, anim_player_link, mut anim_state, anims) in &mut q_models {
+		let enemy_state = q_enemies.get(parent.get()).unwrap();
+
+		let mut anim_player = q_anim_player.get_mut(anim_player_link.0).unwrap();
+
+		let new_anim_state = match enemy_state {
+			EnemyState::Idle => AnimationState::Idle,
+			EnemyState::Attack(_) => AnimationState::Run,
+		};
+		println!("{:?}", new_anim_state);
+
+		if new_anim_state != *anim_state {
+			*anim_state = new_anim_state;
+			anim_player
+				.play_with_transition(
+					anims.0[&*anim_state].clone_weak(),
+					Duration::from_millis(200),
+				)
 				.repeat();
 		}
 	}
